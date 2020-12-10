f61e072f72dddd9ee7a78781b5d287f3110289e3
Markus Perndorfer <mpern@users.noreply.github.com>
2020-12-10 13:47:23 +0100

    Squashed 'core-customize/hybris/bin/custom/extras/sanecleanup/' content from commit 6705796
    git-subtree-dir: core-customize/hybris/bin/custom/extras/sanecleanup
    git-subtree-split: 670579652c8eef764daa084b67b8686fb84e73be

---
+++ src/mpern/sap/cleanup/constants/SanecleanupConstants.java
@@ -0,0 +1,8 @@
+package mpern.sap.cleanup.constants;
+
+public final class SanecleanupConstants extends GeneratedSanecleanupConstants {
+    public static final String EXTENSIONNAME = "sanecleanup";
+
+    private SanecleanupConstants() {
+    }
+}
