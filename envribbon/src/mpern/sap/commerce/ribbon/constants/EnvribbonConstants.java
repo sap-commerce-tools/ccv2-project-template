3a67522e29c6807d77940b1766eafb0a46dcca9f
Markus Perndorfer <mpern@users.noreply.github.com>
2020-12-10 13:34:10 +0100

    Squashed 'core-customize/hybris/bin/custom/extras/envribbon/' content from commit d1ed838
    git-subtree-dir: core-customize/hybris/bin/custom/extras/envribbon
    git-subtree-split: d1ed83876397ebdc7426a6341fb5e59d3d9fcf79

---
+++ envribbon/src/mpern/sap/commerce/ribbon/constants/EnvribbonConstants.java
@@ -0,0 +1,26 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This software is the confidential and proprietary information of SAP
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with SAP.
+ */
+package mpern.sap.commerce.ribbon.constants;
+
+/**
+ * Global class for all Envribbon constants. You can add global constants for your extension into this class.
+ */
+public final class EnvribbonConstants extends GeneratedEnvribbonConstants
+{
+	public static final String EXTENSIONNAME = "envribbon";
+
+	private EnvribbonConstants()
+	{
+		//empty to avoid instantiating this constant class
+	}
+
+	// implement here constants used by this extension
+}
---
+++ envribbonaddon/src/mpern/sap/commerce/ribbon/jalo/EnvRibbonComponent.java
@@ -0,0 +1,25 @@
+package mpern.sap.commerce.ribbon.jalo;
+
+import de.hybris.platform.jalo.Item;
+import de.hybris.platform.jalo.JaloBusinessException;
+import de.hybris.platform.jalo.SessionContext;
+import de.hybris.platform.jalo.type.ComposedType;
+import org.apache.log4j.Logger;
+
+public class EnvRibbonComponent extends GeneratedEnvRibbonComponent
+{
+	@SuppressWarnings("unused")
+	private static final Logger LOG = Logger.getLogger( EnvRibbonComponent.class.getName() );
+	
+	@Override
+	protected Item createItem(final SessionContext ctx, final ComposedType type, final ItemAttributeMap allAttributes) throws JaloBusinessException
+	{
+		// business code placed here will be executed before the item is created
+		// then create the item
+		final Item item = super.createItem( ctx, type, allAttributes );
+		// business code placed here will be executed after the item was created
+		// and return the item
+		return item;
+	}
+	
+}
