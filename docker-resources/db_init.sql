740750620d14c7727e4e87d97713f278e9f22a30
Markus Perndorfer <mpern@users.noreply.github.com>
2021-02-03 11:31:52 +0100

    increase performance of local dev setup

--- docker-resources/db_init.sql
+++ docker-resources/db_init.sql
@@ -1,23 +1,28 @@
-exec sp_configure 'contained database authentication', 1
+EXEC sp_configure 'show advanced options', 1;
+GO
+RECONFIGURE
+GO
+EXEC sp_configure 'cost threshold for parallelism', 50;
+GO
+RECONFIGURE
+GO
+EXEC sp_configure 'max degree of parallelism', 8;
+GO
+RECONFIGURE
+GO
+exec sp_configure 'contained database authentication', 1;
 go
-
 reconfigure
 go
-
 create database localdev containment = partial;
 go
-
 ALTER DATABASE localdev SET READ_COMMITTED_SNAPSHOT ON
 go
-
 ALTER DATABASE localdev SET ALLOW_SNAPSHOT_ISOLATION ON
 GO
-
 USE localdev
 GO
-
 CREATE USER localdev
 WITH PASSWORD = 'localdev1!'
 GO
-
 exec sp_addrolemember 'db_owner', 'localdev'
